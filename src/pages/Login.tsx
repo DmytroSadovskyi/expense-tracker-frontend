8a3894a68817bb8f418ca6fe0eaeeddb35226e08
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2025-03-26 13:39:00 +0200

    change title again

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,7 @@ import { AuthForm } from '../components/AuthForm';
 export const Login = () => {
   return (
     <>
-      <h1>Сторінка логін</h1>
+      <h1>Сторінка логіну</h1>
       <AuthForm type="login" />
     </>
   );
